95beface49917cc59cfb63ce6fe1e5dfb9d7f739
Melven <melvenbagolaque@gmail.com>
2023-09-25 22:41:19 +0800

    fix: minor changes in techstack

--- src/data/featuredProjects.jsx
+++ src/data/featuredProjects.jsx
@@ -1,11 +1,19 @@
 import proj1 from "../assets/images/highground-app.PNG";
+import proj2 from "../assets/images/Educator.JPG";
 
 export const projects = [
   {
     title: 'Highground',
     description: 'is an email-based management system designed specifically for scheduling visits to the beautiful Lake Maragang.',
     image: proj1,
-    techStack: ['PostgreSQL', 'Express', 'React', 'Node.js', 'Tailwind'],
+    techStack: ['React', 'Tailwind', 'PostgreSQL', 'Express', 'Node.js'],
     link: "https://github.com/WoodWalkerr/st-highground-app"
+  },
+  {
+    title: 'Educator',
+    description: 'is a student management application focused on data management and student success optimization, including attendance and learning performance.',
+    image: proj2,
+    techStack: ['React', 'Tailwind', 'PostgreSQL', 'Express', 'Node.js'],
+    link: "https://github.com/chingu-voyages/v45-tier3-team-31"
   },
 ];
