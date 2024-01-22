ef0d1ca0860f995c0db231dbe256201e683c90d2
Melven <melvenbagolaque@gmail.com>
2024-01-22 11:57:26 +0800

    added project

--- src/data/featuredProjects.jsx
+++ src/data/featuredProjects.jsx
@@ -1,13 +1,15 @@
 import proj1 from "../assets/images/highground-app.PNG";
 import proj2 from "../assets/images/Educator.JPG";
+import proj3 from "../assets/images/shopEase.JPG";
+
 
 export const projects = [
   {
-    title: 'Highground',
-    description: 'is an email-based management system designed specifically for scheduling visits to the beautiful Lake Maragang.',
-    image: proj1,
-    techStack: ['React', 'Tailwind', 'PostgreSQL', 'Express', 'Node.js'],
-    link: "https://github.com/WoodWalkerr/st-highground-app"
+    title: 'ShopEase',
+    description: 'ShopEase is an e-commerce website designed for a seamless shopping experience. Explore curated fashion and lifestyle essentials, enjoy secure transactions, easy payments, and top-notch customer service.',
+    image: proj3,
+    techStack: ['NextJs', 'TypeScript', 'Sanity', 'TailwindCss', 'Shadcn/ui'],
+    link: "https://github.com/WoodWalkerr/e-commerce-web"
   },
   {
     title: 'Educator',
@@ -16,4 +18,12 @@ export const projects = [
     techStack: ['React', 'CSS', 'PostgreSQL', 'Express', 'Node.js'],
     link: "https://github.com/chingu-voyages/v45-tier3-team-31"
   },
+  {
+    title: 'Highground',
+    description: 'is an email-based management system designed specifically for scheduling visits to the beautiful Lake Maragang.',
+    image: proj1,
+    techStack: ['React', 'Tailwind', 'PostgreSQL', 'Express', 'Node.js'],
+    link: "https://github.com/WoodWalkerr/st-highground-app"
+  },
+  
 ];
